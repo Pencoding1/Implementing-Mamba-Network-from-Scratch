0b418d021bb267b041d6d4a406da8bc2a63b332b
pencoding1 <duyanh314159@gmail.com>
2025-11-12 00:29:37 +0700

    fix typos

--- Traditional_RNN/RNN.py
+++ Traditional_RNN/RNN.py
@@ -1,18 +1,18 @@
 from typing import Tuple
-from torch import Tensor, zeros
+from torch import Tensor, zeros, stack
 from torch.nn import Module
 from Architecture_cell.RNN_Cell import RNN_Cell
 
 class RNN(Module):
     def __init__(self, 
                  input_dim:int, 
-                 hidde_dim:int, 
+                 hidden_dim:int, 
                  device:str="cpu",
                  bias:bool=True) -> None:
         super().__init__()
-        self.cell = RNN_Cell(input_dim, hidde_dim, bias)
+        self.cell = RNN_Cell(input_dim, hidden_dim, bias)
         self.device = device
-        self.hidden_dim = hidde_dim
+        self.hidden_dim = hidden_dim
         
     def forward(self, sequences:Tensor) -> Tuple[Tensor, Tensor]:
         batch_size, seq_lengths, _ = sequences.shape
@@ -21,10 +21,10 @@ class RNN(Module):
                              device=self.device,
                              dtype=sequences.dtype)
         
-        all_hidden_state = []
+        all_hidden_states = []
         for t in range(seq_lengths):
             x_t = sequences[:, t, :]
-            hidden_state = self.cell.forward(x_t, hidden_state)
-            all_hidden_state.append(hidden_state)
+            hidden_state = self.cell(x_t, hidden_state)
+            all_hidden_states_tensor = stack(all_hidden_states, dim=1)
         
-        return all_hidden_state, hidden_state
+        return all_hidden_states_tensor, hidden_state
